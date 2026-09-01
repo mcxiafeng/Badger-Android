cb99c983e3acdbffdd5f4d7baa2d2075d9d25d35
MC_xiafeng_233 <50166277+mcxiafeng@users.noreply.github.com>
2026-09-01 21:20:43 +0800

    fix(di): wire contact network resolver into detail VM

--- app/src/main/kotlin/top/mcxiafeng/badger/di/KoinModules.kt
+++ app/src/main/kotlin/top/mcxiafeng/badger/di/KoinModules.kt
@@ -179,6 +179,7 @@ val viewModelModule = module {
             aiTagGenerator = get(),
             userProfileTicker = get(),
             avatarStorage = get(),
+            networkResolver = get(),
         )
     }
     viewModel { CreateContactViewModel(get(), get()) }
